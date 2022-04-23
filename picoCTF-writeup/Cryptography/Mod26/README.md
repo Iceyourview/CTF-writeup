e1cb6efb28d4ae00cb94fe241df616662c06e263
XD3an <deanhs0dean@gmail.com>
2022-04-23 21:57:06 +0800

    modify

---
+++ picoCTF-writeup/Cryptography/Mod26/README.md
@@ -0,0 +1,4 @@
+flag = "cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_MAZyqFQj}"
+
+1. Using online decode.
+2. Using program to decode.
---
+++ picoCTF-writeup/Cryptography/Mod26/flag.py
@@ -0,0 +1,12 @@
+flag = "cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_MAZyqFQj}"
+
+AtoZ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+atoz = "abcdefghijklmnopqrstuvwxyz"
+
+for i in flag:
+    if i in AtoZ:
+        print(AtoZ[(AtoZ.index(i)+13)%26],end="")
+    elif i in atoz:
+        print(atoz[(atoz.index(i)+13)%26],end="")
+    else:
+        print(i,end="")

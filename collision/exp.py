3cb2b0a7f08b0e5e0a80e5025b7b579d49f46bbe
XD3an <deanhs0dean@gmail.com>
2022-05-02 00:13:27 +0800

    modify

---
+++ collision/exp.py
@@ -0,0 +1,18 @@
+from pwn import *
+
+sh = ssh('col','pwnable.kr', 2222, 'guest')
+hashcode = 0x21DD09EC
+
+'''
+The input will be checked from argv[1], then enter 'check_password' to calculate the result, and finally return it to main, and then judge whether it is the same as the hashcode, so we need to create the input that is  equal to the hashcode.   
+'''
+# payload
+average = hashcode//5
+remainder = hashcode%5
+
+payload = p32(average)*4 + p32(remainder+average)
+
+# process('./col')
+sh = sh.process(['col', payload], './col')
+sh.interactive()
+
